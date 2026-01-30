3d664b83de2073325be2e92bb54a7d412f06b13f
Shawn Ndombi <shawn@example.com>
2026-01-30 23:45:06 +0300

    Add bcrypt import

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const nodemailer = require('nodemailer');
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 require("dotenv").config();
 const { pool, initDatabase } = require("./database");
 
